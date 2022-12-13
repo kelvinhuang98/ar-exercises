6917b2a911d07a9d12352c61a3883145f48c0399
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-12-13 03:34:29 -0500

    Complete exercise 6

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,8 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Huang", last_name: "Kelvin", hourly_rate: 60)
+@store2.employees.create(first_name: "Cheng", last_name: "Jin-Cheng", hourly_rate: 60)
+@store2.employees.create(first_name: "Zhang", last_name: "Jun Hao", hourly_rate: 60)
